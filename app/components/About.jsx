529e55f380fdd1b1b8cd5b1ec9549a8b3b8380f2
Tim Lisiecki <tim.lisiecki@gmail.com>
2017-04-13 21:06:10 +0000

    update About page style and links

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -2,7 +2,23 @@ var React = require("react");
 
 var About = (props) => {
     return (
-        <h3>About</h3>
+        <div>
+            <h1 className="text-center">About</h1>
+            <p className="text-center">
+                This is a weather application built on React.
+            </p>
+            <ul>
+                <li>
+                    <a href="https://facebook.github.io/react" target="_blank">React</a> - Ued as the JavaScript framework.
+                </li>
+                <li>
+                    <a href="https://openweathermap.org" target="_blank">Open Weather Map</a> - Used as the weather API.
+                </li>
+                <li>
+                    <a href="http://foundation.zurb.com" target="_blank">Foundation</a> - Used to style the application.
+                </li>
+            </ul>
+        </div>
     );
 };
 
