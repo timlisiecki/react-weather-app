1fe686f1f8421047ad211a527efaee3120f17d61
Tim Lisiecki <tim.lisiecki@gmail.com>
2017-04-13 22:27:38 +0000

    add error modal

--- app/api/openWeatherMap.jsx
+++ app/api/openWeatherMap.jsx
@@ -15,9 +15,8 @@ module.exports = {
             } else {
                 return res.data.main.temp;
             }
-        }, function(res) {
-            throw new Error(res.data.message);
+        }, function() {
+            throw new Error("Unable to fetch weather from that location.");
         });
-        
     }
-}
+};
---
+++ app/components/ErrorModal.jsx
@@ -0,0 +1,32 @@
+var React = require("react");
+
+var ErrorModal = React.createClass({
+    getDefaultProps: function() {
+        return {
+            title: "Error"
+        }
+    },
+    componentDidMount: function() {
+        var modal = new Foundation.Reveal($("#error-modal"));
+        modal.open();
+    },
+    propTypes: {
+        title: React.PropTypes.string,
+        message: React.PropTypes.string.isRequired
+    },
+    render: function() {
+        var {title, message} = this.props;
+        
+        return (
+            <div id="error-modal" className="reveal tiny text-center" data-reveal="">
+                <h4>{title}</h4>
+                <p>{message}</p>
+                <p>
+                    <button className="button hollow" data-close="">Okay</button>
+                </p>
+            </div>
+        );
+    }
+});
+
+module.exports = ErrorModal;
